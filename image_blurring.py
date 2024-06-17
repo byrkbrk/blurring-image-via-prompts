9ff00a5e4d7af94ab15e2efd0e63a532407a9548
burak <byrkbrk@gmail.com>
2024-06-17 17:46:22 +0300

    update BlurImage class

--- image_blurring.py
+++ image_blurring.py
@@ -7,5 +7,26 @@ from PIL import Image, ImageFilter
 
 
 class BlurImage(object):
-    def __init__(self):
+    def __init__(self, 
+                 device="mps",
+                 owlvit_ckpt="google/owlv2-base-patch16-ensemble",
+                 owlvit_task="zero-shot-object-detection",
+                 sam_ckpt="mobile_sam.pt",
+                 ):
+        self.module_dir = os.path.dirname(__file__)
+        self.device = self.initialize_device(device)
+        self.owlvit = pipeline(model=owlvit_ckpt, task=owlvit_task, device=self.device)
+        self.sam = SAM(model=sam_ckpt)
+
+    def predict_masks(self, image, prompts):
         pass
+    
+    def predict_boxes(self, image, prompts):
+        pass
+
+    def initialize_device(self, device):
+        pass
+
+
+if __name__ == "__main__":
+    blur_image = BlurImage()
