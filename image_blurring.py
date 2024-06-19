b2057cc6e0500d2a8cd3eec5f30e60abeb4608bc
burak <byrkbrk@gmail.com>
2024-06-19 15:16:12 +0300

    add blur-entire-image method

--- image_blurring.py
+++ image_blurring.py
@@ -22,10 +22,34 @@ class BlurImage(object):
         pass
     
     def predict_boxes(self, image, prompts):
-        pass
+        """Returns bounding boxes for given image and prompts"""
+        return [list(pred["box"].values()) for pred in self.owlvit(image, prompts)]
 
     def initialize_device(self, device):
         pass
+
+    def blur_entire_image(self, image, radius=50):
+        """Returns Gaussian-blurred image"""
+        return image.filter(ImageFilter.GaussianBlur(radius=radius))
+    
+    def annotate_sam_inference(self, inference, area_threshold=0):
+        """Returns list of annotation dicts
+        Args:
+            inference (ultralytics.engine.results.Results): Output of the model
+            area_threshold (int): Threshold for the segmentation area
+        """
+        annotations = []
+        for i in range(len(inference.masks.data)):
+            annotation = {}
+            annotation["id"] = i
+            annotation["segmentation"] = inference.masks.data[i].cpu()==1
+            annotation["bbox"] = inference.boxes.data[i]
+            annotation["score"] = inference.boxes.conf[i]
+            annotation["area"] = annotation["segmentation"].sum()
+
+            if annotation["area"] >= area_threshold:
+                annotations += [annotation]
+        return annotations
 
 
 if __name__ == "__main__":
