0d49598768dce2a9b2eca8692c7e17cf9d7349f6
burak <byrkbrk@gmail.com>
2024-06-20 18:20:32 +0300

    initialize blur.py file

---
+++ blur.py
@@ -0,0 +1,13 @@
+from argparse import ArgumentParser
+from image_blurring import BlurImage
+
+
+
+def parse_arguments():
+    pass
+
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    
