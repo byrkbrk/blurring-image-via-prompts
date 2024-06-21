49b6c78bb5136f8e3f245826b3b9d0c9738c7cf5
burak <byrkbrk@gmail.com>
2024-06-21 18:40:36 +0300

    update blur.py file

--- blur.py
+++ blur.py
@@ -4,10 +4,18 @@ from image_blurring import BlurImage
 
 
 def parse_arguments():
-    pass
-
+    """Returns parsed arguments"""
+    parser = ArgumentParser(description="Blurs image based on given text prompts")
+    parser.add_argument("image_name", type=str, default=None, 
+                        help="Name of the image file that be processed. Image file must be in `images-to-blur` folder")
+    parser.add_argument("text_prompts", nargs="+", type=str, default=None, 
+                        help="Text prompts for the objects that get blurred")
+    parser.add_argument("--image_size", nargs="+", type=int, default=None,
+                        help="Size (width, height) to which the image be transformed. Default: None")
+    parser.add_argument("--device", type=str, default="cpu", help="Device that be used during inference. Default: 'cpu'")
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_arguments()
-    
+    BlurImage(device=args.device).blur(args.image_name, args.text_prompts, size=args.image_size)
