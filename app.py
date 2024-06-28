41949711bbe129a5809572a570891732845dd2f9
burak <byrkbrk@gmail.com>
2024-06-28 17:48:17 +0300

    change default device to None

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from image_blurring import BlurImage
 
 
 if __name__ == "__main__":
-    blur_image = BlurImage()
+    blur_image = BlurImage(device=None)
     gr_interface = gr.Interface(
         fn=lambda image, prompt, save=False: blur_image.blur(image, prompt.split("\n"), save=save),
         inputs=[gr.Image(type="pil"), gr.Textbox(lines=3, placeholder="jacket\ndog head\netc...")],
