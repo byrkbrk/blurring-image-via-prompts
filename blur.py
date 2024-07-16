a9aa1eb5b24ac92864b6ec5b8548480f78212dc7
burak <byrkbrk@gmail.com>
2024-07-16 14:20:54 +0300

    add blur_intensity to blur.py file as argument

--- blur.py
+++ blur.py
@@ -10,6 +10,8 @@ def parse_arguments():
                         help="Name of the image file that be processed. Image file must be in `images-to-blur` folder")
     parser.add_argument("text_prompts", nargs="+", type=str, default=None, 
                         help="Text prompts for the objects that get blurred")
+    parser.add_argument("--blur_intensity", type=int, default=50, 
+                        help="Intensity of the blur that be applied. Default: 50")
     parser.add_argument("--image_size", nargs="+", type=int, default=None,
                         help="Size (width, height) to which the image be transformed. Default: None")
     parser.add_argument("--device", type=str, default="cpu", help="Device that be used during inference. Default: 'cpu'")
@@ -18,4 +20,4 @@ def parse_arguments():
 
 if __name__ == "__main__":
     args = parse_arguments()
-    BlurImage(device=args.device).blur(args.image_name, args.text_prompts, size=args.image_size)
+    BlurImage(device=args.device).blur(args.image_name, args.text_prompts, args.blur_intensity, size=args.image_size)
