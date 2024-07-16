425ceb0c099de7443d047dbe5d96ee9f7a03050a
burak <byrkbrk@gmail.com>
2024-07-16 14:30:05 +0300

    add blur_intesity as slider

--- app.py
+++ app.py
@@ -6,8 +6,11 @@ from image_blurring import BlurImage
 if __name__ == "__main__":
     blur_image = BlurImage(device=None)
     gr_interface = gr.Interface(
-        fn=lambda image, prompt, save=False: blur_image.blur(image, prompt.split("\n"), save=save),
-        inputs=[gr.Image(type="pil"), gr.Textbox(lines=3, placeholder="jacket\ndog head\netc...")],
-        outputs=gr.Image(type="pil")
+        fn=lambda image, prompt, intensity, save=False: blur_image.blur(image, prompt.split("\n"), intensity, save=save),
+        inputs=[gr.Image(type="pil"), 
+                gr.Textbox(lines=3, placeholder="jacket\ndog head\netc..."),
+                gr.Slider(minimum=0, maximum=400, step=10, value=50, label="Blur intensity")],
+        outputs=gr.Image(type="pil"),
+        title="Blur Objects by Prompts"
     )
     gr_interface.launch()
