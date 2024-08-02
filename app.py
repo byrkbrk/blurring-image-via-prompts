ee1123a48a31daf94e1f954d66224cf047cb9be7
burak <byrkbrk@gmail.com>
2024-08-03 00:06:03 +0300

    update app.py file

--- app.py
+++ app.py
@@ -8,20 +8,22 @@ if __name__ == "__main__":
     blur_image = BlurImage(device=None)
     gr_interface = gr.Interface(
         fn=lambda image, prompt, intensity, save=False: blur_image.blur(image, prompt.split("\n"), intensity, save=save),
-        inputs=[gr.Image(type="pil"), 
-                gr.Textbox(lines=3, placeholder="jacket\ndog head\netc..."),
+        inputs=[gr.Image(type="pil", label="Image"), 
+                gr.Textbox(lines=3, placeholder="jacket\ndog head\netc...", label="Prompt"),
                 gr.Slider(minimum=0, maximum=400, step=10, value=50, label="Blur intensity")],
-        outputs=gr.Image(type="pil"),
+        outputs=gr.Image(type="pil", label="Output"),
         title="Blur Objects by Prompts",
         examples=[
             [os.path.join(os.path.dirname(__file__), 
                           "images-to-blur", 
                           "dogs.jpg"),
-             "jacket\ndog head"],
+             "jacket",
+             50],
              [os.path.join(os.path.dirname(__file__), 
                           "images-to-blur", 
                           "hat_sunglasses.jpg"),
-              "hat\nsunglasses"]             
+              "hat\nsunglasses",
+              150]             
         ]
     )
     gr_interface.launch()
