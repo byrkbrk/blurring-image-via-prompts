7bcb28472313ef79c3d53ee94d87469fc8c97bfd
burak <byrkbrk@gmail.com>
2024-09-07 14:10:34 +0300

    upload api.py file

---
+++ api.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, UploadFile, File, Form
+from image_blurring import BlurImage
+from PIL import Image
+
+
+
+app = FastAPI()
+blur_image = BlurImage(None)
+
+@app.get("/")
+def root():
+    return {"message": "Blur objects by prompts"}
+
+@app.post("/blur-image")
+async def blur(file: UploadFile = File(...), 
+                     prompt: str = Form(...),
+                     blur_intensity: int = Form(...)):
+    #print(file, prompt, blur_intensity)
+    prompt = prompt.split(",")
+    print(prompt)
+    blur_image.blur(Image.open(file.file), 
+                    prompt,
+                    blur_intensity,
+                    )
+    return {"filename": file.filename, 
+            "prompt": prompt, 
+            "blur_intensity": blur_intensity}
